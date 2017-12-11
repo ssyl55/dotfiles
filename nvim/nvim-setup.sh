27f6e1f73c0cb788f1aede792522cddbb9ae6e94
Stephen Liu <ssl76@case.edu>
2017-12-11 13:03:28 -0500

    Combine install-vimplug.sh into nvim-setup.sh

--- nvim/nvim-setup.sh
+++ nvim/nvim-setup.sh
@@ -1,3 +1,5 @@
 mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.vim
 echo 'source ~/dotfiles/nvim/init.vim' > ~/.config/nvim/init.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
