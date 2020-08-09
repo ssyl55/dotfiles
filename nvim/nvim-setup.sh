7522c5bb06c7458a61ebca3f14830a5a5dccc449
Stephen Liu <steuyve@gmail.com>
2020-08-09 15:22:16 +0800

    Remove nvim-setup.sh

--- nvim/nvim-setup.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p ~/.config/nvim
-touch ~/.config/nvim/init.vim
-echo 'source ~/dotfiles/nvim/init.vim' > ~/.config/nvim/init.vim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
