16ae886108f18bb9a25a7019f1d3a137fa936b3c
Stephen Liu <steuyve@gmail.com>
2021-05-24 17:09:34 +0800

    Add install_dotfiles script.

---
+++ install_dotfiles.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# git configuration
+mkdir -p ~/.config/git
+cp git/config ~/.config/git/config
+
+# nvim configuration
+mkdir -p ~/.config/nvim
+cp nvim/init.vim ~/.config/nvim/init.vim
+mkdir -p ~/.config/nvim/colors
+cp nvim/colors/molokai.vim ~/.config/nvim/colors/molokai.vim
+
+## install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
