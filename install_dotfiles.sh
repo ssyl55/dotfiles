e3a58d8e0404c5c8aaff820d6653a21e29d9e832
Stephen Liu <steuyve@gmail.com>
2021-05-24 17:12:24 +0800

    Don't break line.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -11,6 +11,5 @@ mkdir -p ~/.config/nvim/colors
 cp nvim/colors/molokai.vim ~/.config/nvim/colors/molokai.vim
 
 ## install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
